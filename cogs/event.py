a63f1db5cdf2fe30e7491be0148f02823ba3b38e
james_su <joan3825ms55@gmail.com>
2023-06-15 21:22:39 +0800

    明天還有DDQAQ

--- cogs/event.py
+++ cogs/event.py
@@ -55,52 +55,52 @@ class Event(commands.Cog):
         word1='笑死'
         word='笑鼠'
         word11='哈哈'
-        url1=['https://imgur.dcard.tw/Fr6zOj0.png','https://memeprod.ap-south-1.linodeobjects.com/user-template/8033ffd8b66053cc9ab07c53d7652654.png']
+        url1=['https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1611839865041.jpg','https://imgur.dcard.tw/Fr6zOj0.png','https://memeprod.ap-south-1.linodeobjects.com/user-template/8033ffd8b66053cc9ab07c53d7652654.png']
         if word1 in message.content or word in message.content or word11 in message.content : 
             x = random.randint(0,len(url1)-1)
             await message.channel.send(f"{url1[x]}")
-            return
+            
     
         word2='窩不知道'
         url2=['https://truth.bahamut.com.tw/s01/201911/4e3eb1c83a9c6204fd1cdcff2206e831.JPG','https://truth.bahamut.com.tw/s01/201907/edaa624f6882cdc725a9bca58659803b.JPG','https://truth.bahamut.com.tw/s01/202004/1cecf7904f5ac8408c555f4ffad83d68.JPG','https://truth.bahamut.com.tw/s01/201902/df15787a89f2db08eaca4d30bda6d707.JPG']
         if word2 in message.content:
             x = random.randint(0,len(url2)-1)
             await message.channel.send(f"{url2[x]}")
-            return
+            
     
         word3='並沒有'
-        url3=['https://memeprod.sgp1.digitaloceanspaces.com/user-resource/a31207a4daab475e4d64655ea6577364.png','https://i.imgur.com/LTMTlj8.jpg']
+        url3=['https://memeprod.sgp1.digitaloceanspaces.com/user-resource/a31207a4daab475e4d64655ea6577364.png','https://i.imgur.com/LTMTlj8.jpg','https://cdn2.ettoday.net/images/3420/d3420288.jpg']
         if word3 in message.content:
             x = random.randint(0,len(url3)-1)
             await message.channel.send(f"{url3[x]}")
-            return
+            
 
         word4='騙人的吧'
         url4=['https://truth.bahamut.com.tw/s01/202105/c4e636d6e6084526102507e29fd3dd27.JPG','https://i.imgur.com/WvUzIEC.jpg']
         if word4 in message.content:
             x = random.randint(0,len(url4)-1)
             await message.channel.send(f"{url4[x]}")
-            return
+            
 
         word5='=U='
         url5=['https://www.u-acg.com/wp-content/uploads/2018/03/yurucam.jpg','https://i.ytimg.com/vi/yG-uaVgKNVM/maxresdefault.jpg','https://i.imgur.com/vt43dre.jpg']
         if word5 in message.content:
             x = random.randint(0,len(url5)-1)
             await message.channel.send(f"{url5[x]}")
-            return
+            
 
         word6='你好棒'
         url6=['https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1633946494585.jpg','https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1611827428231.jpg','https://megapx-assets.dcard.tw/images/0401c23d-cfce-41a5-8f3c-eaddee3276d9/640.jpeg','https://i.imgur.com/c1K37Br.jpg','http://memenow.cc/wp-content/uploads/2020/04/20200409_5e8e8809efa5d.jpg','https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1618457624593.jpg']
         if(word6 in message.content):
             x = random.randint(0,len(url6)-1)
             await message.channel.send(f"{url6[x]}")
-            return
+            
 
         word7='挖堀挖堀'
         word71='挖苦挖苦'
         if(word7 in message.content or word71 in message.content):
             await message.channel.send(f"https://i.ytimg.com/vi/RJA3sLMJ_5M/maxresdefault.jpg")
-            return
+            
 
         word8='安妮亞'
         word81='安妮雅'
@@ -110,7 +110,7 @@ class Event(commands.Cog):
         if(word8 in message.content or word81 in message.content):
             x=random.randint(0,len(url8)-1)
             await message.channel.send(f"{url8[x]}")
-            return
+            
         word9='不可以瑟瑟'
         word91='不可以色色'
         url9=["https://static.wikia.nocookie.net/evchk/images/d/d0/%E4%B8%8D%E5%8F%AF%E4%BB%A5%E8%89%B2%E8%89%B2.jpg/revision/latest?cb=20211013194012","https://dvblobcdnjp.azureedge.net//Content/Upload/Popular/Images/2021-09/637c0cfc-3736-4b76-81ce-9dad2226aab9_m.jpg","https://static.wikia.nocookie.net/evchk/images/0/07/%E4%B8%8D%E5%8F%AF%E4%BB%A5%E8%89%B2%E8%89%B24.jpg/revision/latest/scale-to-width-down/250?cb=20211016193637","https://dvblobcdnjp.azureedge.net//Content/ueditor/net/upload1/2021-09/e967dedb-5683-493d-a5c2-dfb30f55bbe1.png"
@@ -118,21 +118,28 @@ class Event(commands.Cog):
         if(word9 in message.content or word91 in message.content):
             x=random.randint(0,len(url9)-1)
             await message.channel.send(f"{url9[x]}")
-            return
+            
 
         word10='傻眼'
         url10=["https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1588907260330.jpg","https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1649160772882.jpg","https://memeprod.ap-south-1.linodeobjects.com/user-template/4d21d4175ce3cf317d1a532d7cde1550.png"]
         if(word10 in message.content):
             x=random.randint(0,len(url10)-1)
             await message.channel.send(f"{url10[x]}")
-            return
+            
+        
+        word11='不要瞎掰好嗎'
+        url11=["https://cdn2.ettoday.net/images/3420/d3420289.jpg"]
+        if(word11 in message.content):
+            x=random.randint(0,len(url11)-1)
+            await message.channel.send(f"{url11[x]}")
+            
 
         word48763='星爆'
         url48763=['http://i.imgur.com/eH9zKS0.jpg','https://c.tenor.com/4Wmrjus9r0MAAAAC/%E6%98%9F%E7%88%86%E6%B0%A3%E6%B5%81%E6%96%AC.gif','https://x.nctu.app/img/g7Fx.jpg','http://i.imgur.com/EaIXyCA.jpg']
         if word48763 in message.content:
             x = random.randint(0,len(url48763)-1)
             await message.channel.send(f"{url48763[x]}")
-            return
+            
 
 
 async def setup(bot):
