2d80a96275adc60fdf893ee3de01556fe8b63b67
jamessu-desktop <joan3825ms55@gmail.com>
2023-06-17 05:14:44 +0800

    no_rest

--- cogs/auto.py
+++ cogs/auto.py
@@ -31,7 +31,7 @@ class Auto(commands.Cog):
         await channel.send('<@&1104820254145790133>')
         await channel.send('https://giphy.com/gifs/xTcnSSsbe4hhZBvV6M')
     
-    @tasks.loop(second=60)
+    @tasks.loop(seconds=60)
     async def no_rest(self):
         print("no_rest")
         
