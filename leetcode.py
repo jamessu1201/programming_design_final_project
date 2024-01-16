8e8f16662d156425cb51b616963fded121e23c35
jamessuazure <joan3825ms55@gmail.com>
2024-01-16 15:29:52 +0000

    modify leetcode to use request

--- leetcode.py
+++ leetcode.py
@@ -1,11 +1,6 @@
 import platform
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-
-from selenium.webdriver.support.ui import WebDriverWait,Select
-from selenium.webdriver.support import expected_conditions as EC
+import requests
 
 import datetime
 
@@ -13,83 +8,51 @@ import time
 
 
 
-def init():
-	options = Options()
-	options.add_argument("--headless")
-	options.add_argument("--disable-dev-shm-usage"); # overcome limited resource problems
-	options.add_argument('--window-size=1920,1080')
-	options.add_argument("--no-sandbox")
-	options.add_argument("--disable-gpu")
-	options.add_experimental_option("excludeSwitches", ["enable-automation"])
-	options.add_experimental_option('useAutomationExtension', False)
-	
-	options.add_experimental_option("prefs", {"profile.password_manager_enabled": False, "credentials_enable_service": False})
-	if(platform.system()=='Windows'):
-		options.binary_location = "chrome-win64/chrome.exe"
-	elif(platform.system()=='Linux'):
-		options.binary_location = "chrome-linux64/chrome"
-
- 
-	
-
-
-	if(platform.system()=='Windows'):
-		driver = webdriver.Chrome(options=options, executable_path="driver/chromedriver.exe",)
-	elif(platform.system()=='Linux'):
-		driver = webdriver.Chrome(options=options, executable_path="chromedriver-linux64/chromedriver",)
-	
- 
-	driver.delete_all_cookies() #清cookie
- 
- 
-	return driver
 
 
 weekdays={'0':'Mon','1':'Tue','2':'Wed','3':'Thu','4':'Fri','5':'Sat','6':'Sun'}
 
 months={'1':'Jan','2':'Feb','3':'Mar','4':'Apr','5':'May','6':'Jun','7':'Jul','8':'Aug','9':'Sep','10':'Oct','11':'Nov','12':'Dec'}
 
+link=''
+
+def get_result():
+    
+    
+    url="https://leetcode.com/graphql/"
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'}
+
+    params={"query":"\n query questionOfToday {\n activeDailyCodingChallengeQuestion {\n date\n userStatus\n link \n question { \n acRate \n difficulty \n freqBar \n frontendQuestionId: questionFrontendId \n isFavor \n paidOnly: isPaidOnly \n status \n title \n titleSlug \n hasVideoSolution \n hasSolution \n topicTags { \n name  \n id \n slug  \n } \n } \n } \n }\n   "}
+    return requests.get(url,params=params,headers=headers)
+
+def get_link():
+    
+    return link
 
 
 
 def main():
+    
     now=datetime.datetime.now()
+    
+    result=get_result()
 
+    j=result.json()
 
-    result=init()
- 
- 
-    driver=result
+    question=j['data']['activeDailyCodingChallengeQuestion']['question']
 
+    title=question['title']
+    
+    link="https://leetcode.com"+j['data']['activeDailyCodingChallengeQuestion']['link']
+    print(link)
 
-    
-    driver.get("https://leetcode.com/problemset/")
-    
-    with open('aaa.html','w+',encoding='UTF-8') as o:
-        o.write(driver.find_element(By.TAG_NAME, 'body').get_attribute('innerHTML'))
-    
-    print("get to leetcode complete")
+    id=question['frontendQuestionId']
 
-    time.sleep(0.5)
-
+    print(id+'. '+title)
     
     
-    question=driver.find_element(By.XPATH,f"//a[@data-value='{weekdays[str(now.weekday())]} {months[str(now.month)]} {str(now.day).zfill(2)} {str(now.year)} 00:00:00 GMT+0000 (Coordinated Universal Time)']")
-    #question=driver.find_element(By.XPATH,f"//a[@data-value='{weekdays[str(now.weekday())]} {months[str(now.month)]} {str(now.day).zfill(2)} {str(now.year)} 00:00:00 GMT+0800 (台北標準時間)']") #windows
+    title=id+'. '+title
     
-    
-    print(question.get_attribute('href'))
-    
-    driver.get(question.get_attribute('href'))
-
-    time.sleep(0.5)
-    
-    with open('bbb.html','w+',encoding='UTF-8') as o:
-    	o.write(driver.page_source)
-    
-    title=driver.title
-    print(title[0:title.find('-')])
     return str(now.month)+"/"+str(now.day)+" "+title
 
-
-#main()
+main()
