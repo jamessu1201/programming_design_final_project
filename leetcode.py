36afda20f14668c71e16a2b521ebf8ae5fd2417b
jamessuazure <joan3825ms55@gmail.com>
2024-03-07 02:50:41 +0000

    leetcode_with_request

--- leetcode.py
+++ leetcode.py
@@ -13,6 +13,7 @@ weekdays={'0':'Mon','1':'Tue','2':'Wed','3':'Thu','4':'Fri','5':'Sat','6':'Sun'}
 
 months={'1':'Jan','2':'Feb','3':'Mar','4':'Apr','5':'May','6':'Jun','7':'Jul','8':'Aug','9':'Sep','10':'Oct','11':'Nov','12':'Dec'}
 
+
 link=''
 
 
@@ -45,6 +46,8 @@ def main():
     j=result.json()
 
     question=j['data']['activeDailyCodingChallengeQuestion']['question']
+    
+    print(question)
 
     title=question['title']
     
@@ -57,7 +60,7 @@ def main():
     
     title=id+'. '+title
     
-    return str(now.month)+"/"+str(now.day)+" "+title
+    return str(now.month)+"/"+str(now.day)+" "+title,question['difficulty']
 
-# print(main())
+print(main())
 # print(get_link())
